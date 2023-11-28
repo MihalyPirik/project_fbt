ac66a00671dbd651c3a845da23426ab09f4688bb
Mihaly Pirik <misipirik@gmail.com>
2023-11-28 09:38:47 +0100

    import scss

--- frontend_book100/src/main.js
+++ frontend_book100/src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import "bootstrap";
+import "./assets/style.scss";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
