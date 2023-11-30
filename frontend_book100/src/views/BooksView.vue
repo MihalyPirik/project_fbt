81043f914667dd3296ea914ef9d5ac25b48a524e
Mihaly Pirik <misipirik@gmail.com>
2023-11-30 11:09:22 +0100

    bookcard

--- frontend_book100/src/views/BooksView.vue
+++ frontend_book100/src/views/BooksView.vue
@@ -2,21 +2,14 @@
   <div class="container">
     <h1 class="display-5 text-center">Könyvek</h1>
     <div class="row">
-      <div class="col-12 col-md-3" v-for="book in books">
-        <div class="card">
-          <img src="" class="card-img-top" alt="..." />
+      <div class="col-12 col-md-3 mb-2" v-for="book in books">
+        <div class="card h-100">
+          <img :src="imgUrl + book.ImageName" class="card-img-top" alt="..." />
           <div class="card-body">
-            <h5 class="card-title">{{book.Title}}</h5>
-            <p class="card-text">
-              Szerző: {{ book.Author }}
-            </p>
-            <p>
-              Nyelv: {{ book.Language }}
-            </p>
-            <p>
-              Ország: {{ book.Country }}
-            </p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">{{ book.Title }}</h5>
+            <p class="card-text">Szerző: {{ book.Author }}</p>
+            <p>Nyelv: {{ book.Language }}</p>
+            <p>Ország: {{ book.Country }}</p>
           </div>
         </div>
       </div>
@@ -28,6 +21,7 @@
 import { ref } from "vue";
 import bookService from "../services/bookservice";
 
+const imgUrl = import.meta.env.VITE_IMG_URL;
 const books = ref();
 
 bookService.getAllBook().then((res) => {
